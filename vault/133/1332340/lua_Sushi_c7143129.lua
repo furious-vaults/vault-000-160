d9d3346e8eadb280af386868eb68487f30566574
furious-vaults <diegohenriquewcl@hotmail.com>
2026-03-25 08:18:40 -0300

    Upload Lote 1 - Prefixo 133

---
+++ vault/133/1332340/lua_Sushi_c7143129.lua
@@ -0,0 +1,8 @@
+addappid(1332340)
+addappid(1332341, 1, "4299a87965c6fa8f20880c1225607076c95cae50666d2ee56b9a19d5cc3e7da2")
+setManifestid(1332341, "3593477581239036899", 0)
+
+--[[
+This file Generated using fares.top Website 
+-----> join discord Server : https://discord.gg/steamgames 
+]]

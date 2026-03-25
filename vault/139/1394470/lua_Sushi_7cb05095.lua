9327d022bf3ee23f23a2a37adf686733dadf7d91
furious-vaults <diegohenriquewcl@hotmail.com>
2026-03-25 08:24:26 -0300

    Upload Lote 2 - Prefixo 139

---
+++ vault/139/1394470/lua_Sushi_7cb05095.lua
@@ -0,0 +1,11 @@
+-- Made by f1uxin, please read the (README) file and enjoy!
+-- P.S. If there is no README file, this file has most likely been stolen the creator of these files is @𓆩𝙁𝟙𝙪𝙭𝙞𝙣𓆪 / username - f1uxin. On discord, nobody else.
+-- If you want to redistribute/share these files, read the README TXT for more info.
+-- Feel free to join my (F1uxins) official server: https://discord.gg/NmmpgnAgen
+-- (We show everything about piracy, including manifest & lua stuff also known as the "Steam method", all for free, Nobody should need to pay for info that is spread and shown for free.)
+
+-- MAIN APPLICATION
+addappid(1394470) -- Umami Grove
+-- MAIN APP DEPOTS
+addappid(1394471, 1, "8d3158db22f1dff7735792ead9c64b3ec2df6dc50ee9579fd4dfb513288c69c0") -- Depot 1394471
+setManifestid(1394471, "2431160555561010737", 6580840503)

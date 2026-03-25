5a70fe0089d0ab1374f9f0d5d984d04382acb4b2
furious-vaults <diegohenriquewcl@hotmail.com>
2026-03-25 08:06:40 -0300

    Upload Lote 2 - Prefixo 120

---
+++ vault/120/1205970/lua_Sushi_a7049522.lua
@@ -0,0 +1,8 @@
+addappid(1205970)
+addappid(1205971,0,"bda7fa2a1e987629940c3faf629f17a0af5939f54cc244a56b1ac57e29739bbf")
+setManifestid(1205971,"3902687917084328989")
+
+--[[
+This file Generated using fares.top Website 
+-----> join discord Server : https://discord.gg/steamgames 
+]]

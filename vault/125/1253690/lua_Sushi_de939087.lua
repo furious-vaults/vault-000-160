f26d8e7c6d39bae68063f7f916944dbd65ee60f7
furious-vaults <diegohenriquewcl@hotmail.com>
2026-03-25 08:11:04 -0300

    Upload Lote 1 - Prefixo 125

---
+++ vault/125/1253690/lua_Sushi_de939087.lua
@@ -0,0 +1,10 @@
+-- Made by f1uxin, enjoy!
+-- Redistribution/sharing of these files is allowed but you must keep everything as is. 
+-- Feel free to join my(F1uxins) official server: https://discord.gg/f1uxins-ship
+
+-- MAIN APPLICATION
+addappid(1253690) -- Teke Teke - テケテケ
+
+-- MAIN APP DEPOTS
+addappid(1253691, 1, "749d95f6bb06981f740138285e0c4fef787fc22d3c9645008ccc4c90cd2f9c9b") -- Teke Teke - テケテケ Content
+setManifestid(1253691, "1828792389967898454", 0)

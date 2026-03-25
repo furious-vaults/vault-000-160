abf9416e082f78744b8a5a673964506db5760122
furious-vaults <diegohenriquewcl@hotmail.com>
2026-03-25 08:39:26 -0300

    Upload Lote 2 - Prefixo 156

---
+++ vault/156/1568530/lua_Sushi_7194a761.lua
@@ -0,0 +1,20 @@
+-- Made by f1uxin, enjoy!
+-- Redistribution/sharing of these files is allowed but you must keep everything as is. 
+-- Feel free to join my(F1uxins) official server: https://discord.gg/f1uxins-ship
+
+-- MAIN APPLICATION
+addappid(1568530) -- Realife Simulator
+
+-- MAIN APP DEPOTS
+addappid(1577488, 1, "bd3061f2765da69f12a61c85cb4874e876c7f05b992c43861e5d363748bf996e") -- Depot 1577488
+setManifestid(1577488, "190585852605473546", 0)
+
+-- SHARED DEPOTS (from other apps)
+addappid(228984, 1, "df7df98d3134f5e0009d5ebcaaf3bbb91ea2f7cbad81a37a9d16bc1835f57068") -- VC 2012 Redist (Shared from App 228980)
+setManifestid(228984, "2547553897526095397", 0)
+addappid(228985, 1, "21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c") -- VC 2013 Redist (Shared from App 228980)
+setManifestid(228985, "3966345552745568756", 0)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "550968249685141759", 0)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 0)
